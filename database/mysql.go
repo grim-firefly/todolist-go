6ed59c6c8461e9594d3cd418fb485232d5c54e80
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2023-09-15 23:33:06 +0600

    Added .env

--- database/mysql.go
+++ database/mysql.go
@@ -1,1 +1,34 @@
 package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// DB is the connection handle
+var DB *gorm.DB
+
+// ConnectDB connects to the database
+func Connect() {
+	godotenv.Load()
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	db := os.Getenv("MYSQL_DATABASE")
+	username := os.Getenv("MYSQL_USERNAME")
+	password := os.Getenv("MYSQL_PASSWORD")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, db)
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	DB = d
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
--- main.go
+++ main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/grim-firefly/todolist-go/database"
+)
+
+func main() {
+	database.Connect()
+}
